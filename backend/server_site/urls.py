ce1a4a48d68978998e9f5b4ea1f6be54243b2fa0
Dylan Coxshall <dacoxshall@gmail.com>
2023-06-07 17:31:07 +0100

    Add virtual environment and requirements.txt

--- backend/server_site/urls.py
+++ backend/server_site/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('mysite.urls')),
+    path('dylsite/', include('mysite.urls')),
     path('admin/', admin.site.urls),
 ]
