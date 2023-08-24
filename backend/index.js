21793e7f3db3213b80177f4b8aa1dfa7bf338833
Dylan Coxshall <dacoxshall@gmail.com>
2023-08-24 16:46:26 +0100

    Add react-app, alter server to serve build folder in react-app

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "..", "react-app", "build")));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Example app listening on ${port}`);
+})
