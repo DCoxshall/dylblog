6055a5be69d24bc2e6d221e3fc18c0cd6b8b70ec
Dylan Coxshall <dacoxshall@gmail.com>
2023-08-25 10:30:52 +0100

    Add home link to Navbar component

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends React.Component {
     render() {
         return (
             <div className="navbar">
+                <Link to="/">home</Link>
                 <Link to="/projects">projects</Link>
                 <Link to="/pdf.pdf" target="_blank">resume</Link>
             </div>
