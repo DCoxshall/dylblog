f93c0db0aee3a6c07843a1f883bf760ee4ae3fb8
Dylan Coxshall <dacoxshall@gmail.com>
2023-09-04 20:44:51 +0100

    Fixed pages not reloading: search 'react-router reload page' for more information

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,14 @@ const port = 80;
 app.use(express.static(path.join(__dirname, "..", "react-app", "build")));
 app.use(express.static("public"));
 
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "react-app", "build/index.html"), (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on ${port}`);
 })
