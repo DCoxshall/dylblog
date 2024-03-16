7291773de7f0f67d4c42d0d4ce19620b1cf352bc
DCoxshall <dacoxshall@gmail.com>
2024-03-16 16:15:46 +0000

    Add responsive design to homepage.

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import "../App.css";
 
 function Layout() {
     return (
-        <div>
+        <div id="main-content">
             <Navbar />
             <hr />
             <Outlet />
