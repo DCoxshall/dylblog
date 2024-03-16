6b21e5407495ebf6d544a8727aec2886b721eac6
DCoxshall <dacoxshall@gmail.com>
2024-03-16 14:57:12 +0000

    change server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const process = require('process');
+const fs = require('fs');
+
+const PORT = 80;
 
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
-app.listen(80);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("Unknown error in server setup.");
+  } else {
+    console.log("Server listening on port:", PORT);
+  }
+});
